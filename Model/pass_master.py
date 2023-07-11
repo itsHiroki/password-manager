6cceb2eecda0912bc67da4572fc67eb6c6e04aa5
itsHiroki <ianfanfair@protonmail.com>
2023-07-11 10:11:23 +0200

    converted to a class and commented

--- Model/pass_master.py
+++ Model/pass_master.py
@@ -1,29 +1,38 @@
+#Terminal version of the password manager
+
+#imports
 import user
 import database_protocol
 
 
-def login():
-    pass
+class PassMaster:
+    '''Function calls used by the user'''
+    def __init__(self):
+        pass
+
+    def login(self):
+        pass
 
 
-def create_new_user(username, password):
-    return user.User(username, password, 0)
+    def create_new_user(self,username, password):
+        return user.User(username, password, 0)
 
-def new_user():
-    username = input ('Enter username: ')
-    password = input ('Enter password: ')
+    def new_user(self):
+        username = input ('Enter username: ')
+        password = input ('Enter password: ')
 
-    if(username != '' and password != ''):
-        return create_new_user(username, password)
-    else:
-        new_user()
+        if(username != '' and password != ''):
+            return self.create_new_user(username, password)
+        else:
+            self.new_user()
 
 def main():
+    pm = PassMaster()
     option = input("Welcome to PassMaster \nPress 1 to login or 2 to create a new user: ")
     if option == '1':
-        login()
+        pm.login()
     elif option == '2':
-        user = new_user()
+        user = pm.new_user()
     else:
         main()
 
